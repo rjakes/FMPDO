a8d1d60ebc3448525151587b2b3332989c26f68a
Roger Jacques <roger@rjakes.com>
2013-12-10 22:51:32 -0800

    add Record->setField, add Record->commit()

--- library/rjakes/FMPDO/Record.php
+++ library/rjakes/FMPDO/Record.php
@@ -53,6 +53,14 @@ class Record
     }
 
 
+    function setField($field, $value, $repetition= 0)
+    {
+
+            $this->fields[$field][$repetition] = $value;
+
+    }
+
+
     function getRecordId()
     {
         if(isset($this->recordid)){
@@ -61,4 +69,47 @@ class Record
             return FALSE;
         }
     }
+
+    function commit()
+    {
+        $table = $this->table;
+        $id = $this->recordid;
+        $db = FMPDO::getConnection();
+
+        $colum_nv = array();
+        foreach($this->fields as $k => $v)
+         {
+             if($k != 'id')
+             {
+                $column_nv[$k] = $v[0];
+             }
+         }
+
+
+
+        if($id===FALSE)
+        {
+            // this is a new record, need to insert
+            $columnString = implode(',', array_keys($column_nv));
+            $valueString = implode(',', array_fill(0, count($column_nv), '?'));
+            $query = $db->prepare("INSERT INTO ".$table." ({$columnString}) VALUES ({$valueString})");
+        }else{
+          // this is an existing record, need to update
+            $setString = "";
+            foreach($column_nv as $k=>$v)
+            {
+                $setString .= $k."=?,";
+            }
+            $setString = substr($setString, 0, -1);
+
+            $query = $db->prepare("UPDATE ".$table."  SET {$setString} WHERE id=".$id);
+
+        }
+        $query->execute(array_values($column_nv));
+
+
+
+    }
+
+
 }
